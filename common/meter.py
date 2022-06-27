5508fc6cd65e337f70e0ad500b4670c91c3f65f1
gkgo <91817417+gkgo@users.noreply.github.com>
2022-06-27 18:28:56 +0800

    Update meter.py

--- common/meter.py
+++ common/meter.py
@@ -22,3 +22,26 @@ class Meter:
 
     def avg_and_confidence_interval(self):
         return self.avg(), self.confidence_interval()
+
+
+class Focal_Loss():
+    def __init__(self, weight, gamma=2):
+        super(Focal_Loss, self).__init__()
+        self.gamma = gamma
+        self.weight = weight
+
+    def forward(self, preds, labels):
+        """
+        preds:softmax输出结果
+        labels:真实值
+        """
+        eps = 1e-7
+        y_pred = preds.view((preds.size()[0], preds.size()[1], -1))  # B*C*H*W->B*C*(H*W)
+
+        target = labels.view(y_pred.size())  # B*C*H*W->B*C*(H*W)
+
+        ce = -1 * torch.log(y_pred + eps) * target
+        floss = torch.pow((1 - y_pred), self.gamma) * ce
+        floss = torch.mul(floss, self.weight)
+        floss = torch.sum(floss, dim=1)
+        return torch.mean(floss)

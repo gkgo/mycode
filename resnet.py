254cf14da070470bf2b380ac4baa6e1edab05468
gkgo <91817417+gkgo@users.noreply.github.com>
2023-02-27 20:21:28 +0800

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -200,9 +200,19 @@ class ResNet(nn.Module):
 def resnet12():
     return ResNet(BasicBlock)
 
+
+
+def conv_block(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, 3, padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(),
+        nn.MaxPool2d(2)
+    )
+
 class ConvNet4(nn.Module):
 
-    def __init__(self, num_classes=100, x_dim=3, hid_dim=64, z_dim=640):
+    def __init__(self, num_classes=5, x_dim=3, hid_dim=64, z_dim=640):
         super().__init__()
         self.encoder = nn.Sequential(
             conv_block(x_dim, hid_dim),

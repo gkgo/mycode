794ff8cf3e0e66d331e45938a52bb4cbf4107ea9
gkgo <91817417+gkgo@users.noreply.github.com>
2023-02-28 16:20:16 +0800

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -5,6 +5,52 @@ import torch.nn.functional as F
 import torch
 import torch.nn as nn
 
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=8):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        # 利用1x1卷积代替全连接
+        self.fc1   = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2   = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=3):
+        super(SpatialAttention, self).__init__()
+
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
+
+class cbam_block(nn.Module):
+    def __init__(self, channel, ratio=8, kernel_size=3):
+        super(cbam_block, self).__init__()
+        self.channelattention = ChannelAttention(channel, ratio=ratio)
+        self.spatialattention = SpatialAttention(kernel_size=kernel_size)
+
+    def forward(self, x):
+        x = x * self.channelattention(x)
+        x = x * self.spatialattention(x)
+        return x
+
 class SqueezeExcitation(nn.Module):
     def __init__(self, channel, reduction=16):
         super(SqueezeExcitation, self).__init__()
@@ -249,7 +295,8 @@ class ConvNet4(nn.Module):
         )
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(640, num_classes)
-        self.scr_module = mySelfCorrelationComputation(kernel_size=(1, 1), padding=0)
+        self.scr_module = cbam_block(channel=640)
+#         self.scr_module = mySelfCorrelationComputation(kernel_size=(1, 1), padding=0)
 #         self.scr_module = SqueezeExcitation(channel=640)
 
     def forward(self, x):
@@ -267,23 +314,24 @@ class ConvNet4(nn.Module):
         
 
 
-        b, c, h, w = x.shape
-        x = normalize_feature(x)
+#         b, c, h, w = x.shape
+#         x = normalize_feature(x)
         
-        y = F.normalize(x, p=2, dim=1, eps=1e-8)
+#         y = F.normalize(x, p=2, dim=1, eps=1e-8)
         
-        d_s = y.view(b, c, -1)
-        d_s = gaussian_normalize(d_s, dim=2)
+#         d_s = y.view(b, c, -1)
+#         d_s = gaussian_normalize(d_s, dim=2)
         
-        d_s = F.softmax(d_s /5.0, dim=2)
-        d_s = d_s.view(b,c,h, w)
+#         d_s = F.softmax(d_s /5.0, dim=2)
+#         d_s = d_s.view(b,c,h, w)
         
-        x1 = d_s + x
-                
-#         x = self.avgpool(x1)
-#         x = x.view(x.size(0), -1)
+#         x1 = d_s + x
+#         x = x1.mean(dim=[-1, -2])
 #         x = self.fc(x)
-        
-        x = x1.mean(dim=[-1, -2])
+                
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
         x = self.fc(x)
+        
+
         return x
